0dd03b1d2bc87d692794f6fe8747460864dde2af
xingxing3 <xingxing@jd.com>
2022-09-19 10:25:11 +0800

    chore(i18n): change the options's scope

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2419,11 +2419,13 @@ const options: RenovateOptions[] = [
     description: `User-facing text language`,
     type: 'string',
     default: 'en',
+    globalOnly: true,
   },
   {
     name: 'translationsFilePath',
     description: `PO (Portable Object) file's path`,
     type: 'string',
+    globalOnly: true,
   },
 ];
 
