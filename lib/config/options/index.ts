46aa2dd7d194eb1c0175fb3b293d4e2490ab235e
xingxing3 <xingxing@jd.com>
2022-09-21 17:35:23 +0800

    format(i18n): correct misspelling

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2436,7 +2436,7 @@ const options: RenovateOptions[] = [
   },
   {
     name: 'locale',
-    description: `User-facing text language`,
+    description: 'User-facing text language',
     type: 'string',
     default: 'en',
     globalOnly: true,
