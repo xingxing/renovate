55f0611e89c7f1535253e45ea92023c8a730d1a8
xingxing3 <xingxing@jd.com>
2022-09-28 18:05:53 +0800

    chore(i18n): worker relevant translations

--- lib/workers/repository/update/pr/body/controls.ts
+++ lib/workers/repository/update/pr/body/controls.ts
@@ -1,3 +1,4 @@
+import { _ } from '../../../../../i18n';
 import { emojify } from '../../../../../util/emoji';
 import { isBranchModified } from '../../../../../util/git';
 import type { BranchConfig } from '../../../../types';
@@ -6,5 +7,7 @@ export async function getControls(config: BranchConfig): Promise<string> {
   const warning = (await isBranchModified(config.branchName))
     ? emojify(' :warning: **Warning**: custom changes will be lost.')
     : '';
-  return `\n\n---\n\n - [ ] <!-- rebase-check -->If you want to rebase/retry this PR, click this checkbox.${warning}\n\n`;
+  return `\n\n---\n\n - [ ] <!-- rebase-check -->${_(
+    'If you want to rebase/retry this PR, click this checkbox.'
+  )}${warning}\n\n`;
 }
