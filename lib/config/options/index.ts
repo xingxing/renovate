0d9686468be36b024ce3acbae1cef37a215fed64
Wade Xing <xing@big.one>
2022-10-04 15:10:09 +0800

    refactor(i18n): remove locale option
    get locale from the PO file

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2421,13 +2421,6 @@ const options: RenovateOptions[] = [
     default: false,
   },
   {
-    name: 'locale',
-    description: 'User-facing text language',
-    type: 'string',
-    default: 'en',
-    globalOnly: true,
-  },
-  {
     name: 'translationsFilePath',
     description: `PO (Portable Object) file's path`,
     type: 'string',
